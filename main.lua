9477727433ebdae7f1bacf60f5ba48a01308ef46
Paweł Mstowski <pawel.mstowski@gmail.com>
2025-10-20 21:52:47 +0200

    Improve balls generation and move UI to function and draw it last

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ local Bullet = require("src.bullet")
 local Vector = require("src.vector")
 local ww, wh = love.graphics.getDimensions()
 local player = require("src.player"):new(Vector:new(ww / 2, wh - 50))
-local title = love.graphics.newText(love.graphics.newFont(32), "Dodge The Bullets!")
+local title = love.graphics.newText(love.graphics.newFont(32), "Dodge Ball(s)!")
 local points = 0
 local ellapsed_time = 0
 local next_bullet_time = 0
@@ -39,12 +39,13 @@ function love.update(dt)
 	end
 
 	if next_bullet_time <= 0 then
-		local x_pos = math.random(20, ww - 20)
+		local x_pos = math.random(-20, ww + 20)
 		print("Spawning bullet at x: " .. x_pos)
-		local b_pos = Vector:new(x_pos, -50)
+		local b_pos = Vector:new(x_pos, 0)
 		local b = Bullet:new(
 			b_pos,
-			b_pos:direction_to(player.pos:add(Vector:new(math.random(-20, 20), math.random(-20, 20))))
+			-- b_pos:direction_to(player.pos:add(Vector:new(math.random(-20, 20), math.random(-20, 20))))
+			b_pos:direction_to(Vector:new(math.random(-20, ww + 20), wh))
 		)
 		bullets[#bullets + 1] = b
 		next_bullet_time = math.max(0.05, 1.0 - ellapsed_time / 30)
@@ -81,14 +82,17 @@ end
 
 function love.draw()
 	love.graphics.clear(0.1, 0.1, 0.1)
-	-- love.graphics.print("Hello, World! dim: " .. ww .. " x " .. wh, 400, 300)
-	love.graphics.draw(title, ww / 2 - title:getWidth() / 2, 10)
-	local points_text = love.graphics.newText(love.graphics.newFont(24), "Points: " .. math.floor(points))
-	love.graphics.draw(points_text, ww - points_text:getWidth() - 10, 10)
 	for _, b in ipairs(bullets) do
 		b:draw()
 	end
 	player:draw()
+	draw_ui()
+end
+
+function draw_ui()
+	love.graphics.draw(title, ww / 2 - title:getWidth() / 2, 10)
+	local points_text = love.graphics.newText(love.graphics.newFont(24), "Points: " .. math.floor(points))
+	love.graphics.draw(points_text, ww - points_text:getWidth() - 10, 10)
 	if not game_running then
 		local game_over_text = love.graphics.newText(love.graphics.newFont(48), "Game Over! Press 'R' to Restart")
 		love.graphics.draw(
